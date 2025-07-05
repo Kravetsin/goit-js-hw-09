0da8ff3757be37e5a8d50a15cb3d8b93f50a16ec
Kravets <106190014+Kravetsin@users.noreply.github.com>
2025-07-05 12:01:30 +0200

    Improve form validation and localStorage handling
    Removed unused DOM queries, added input trimming, and implemented validation to ensure all fields are filled before submission. Now, email and message are trimmed and stored in localStorage, and the form prevents submission if any field is empty.

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,7 +1,4 @@
 const form = document.querySelector('.feedback-form');
-const input = document.querySelector('.feedback__input');
-const textarea = document.querySelector('.feedback__textarea');
-const submit = document.querySelector('.feedback__button');
 
 const formData = {
   email: '',
@@ -21,8 +18,8 @@ function saveToLocalStorage() {
 document.addEventListener('DOMContentLoaded', () => {
   const saved = localStorage.getItem(STORAGE_KEY);
   if (saved) {
-      const parsed = JSON.parse(saved);
-      
+    const parsed = JSON.parse(saved);
+
     Object.entries(parsed).forEach(([key, value]) => {
       formData[key] = value;
       if (form.elements[key]) {
@@ -35,7 +32,7 @@ document.addEventListener('DOMContentLoaded', () => {
 form.addEventListener('input', event => {
   const { name, value } = event.target;
   if (name in formData) {
-    formData[name] = value;
+    formData[name] = value.trim();
     saveToLocalStorage();
   }
 });
@@ -46,16 +43,20 @@ form.addEventListener('submit', event => {
   const formData = new FormData(form);
 
   const email = formData.get('email');
-  localStorage.setItem('email', email);
+  const message = formData.get('message');
+
+  if (email.trim() === '' || message.trim() === '') {
+    alert('Fill please all fields');
+    return;
+  }
+
+  localStorage.setItem('email', email.trim());
   console.log('Email:', email);
 
-  const message = formData.get('message');
   console.log('Message:', message);
-  localStorage.setItem('message', message);
+  localStorage.setItem('message', message.trim());
 
   localStorage.removeItem(STORAGE_KEY);
 
   form.reset();
 });
-
-
