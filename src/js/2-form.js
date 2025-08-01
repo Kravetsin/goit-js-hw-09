a9069930d33ba26e0ab4366268459c59eb9858e3
Kravets <106190014+Kravetsin@users.noreply.github.com>
2025-08-01 14:42:18 +0200

    Fix form

--- src/js/2-form.js
+++ src/js/2-form.js
@@ -1,19 +1,10 @@
 const form = document.querySelector('.feedback-form');
+const STORAGE_KEY = 'feedback-form-state';
 
 const formData = {
   email: '',
   message: '',
 };
-
-const STORAGE_KEY = 'feedback-form-state';
-
-form.addEventListener('input', event => {
-  localStorage.setItem(STORAGE_KEY, event.target.value);
-});
-
-function saveToLocalStorage() {
-  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
-}
 
 document.addEventListener('DOMContentLoaded', () => {
   const saved = localStorage.getItem(STORAGE_KEY);
@@ -31,32 +22,27 @@ document.addEventListener('DOMContentLoaded', () => {
 
 form.addEventListener('input', event => {
   const { name, value } = event.target;
+
   if (name in formData) {
     formData[name] = value.trim();
-    saveToLocalStorage();
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
   }
 });
 
 form.addEventListener('submit', event => {
   event.preventDefault();
 
-  const formData = new FormData(form);
+  const { email, message } = formData;
 
-  const email = formData.get('email');
-  const message = formData.get('message');
-
-  if (email.trim() === '' || message.trim() === '') {
+  if (email === '' || message === '') {
     alert('Fill please all fields');
     return;
   }
 
-  localStorage.setItem('email', email.trim());
-  console.log('Email:', email);
-
-  console.log('Message:', message);
-  localStorage.setItem('message', message.trim());
+  console.log('Submitted data:', formData);
 
   localStorage.removeItem(STORAGE_KEY);
-
   form.reset();
+  formData.email = '';
+  formData.message = '';
 });
